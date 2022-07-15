cb89d8f627a65a3c27a6fc834d9e877d96b6be40
jzapata <JulianAndresZB@gmail.com>
2022-07-15 00:13:51 -0500

    config: basic vue router 4 configuration

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>This is the About component</h1>
+
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>This is the Home component</h1>
+
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
